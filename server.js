62bafbce11759ade319c0f636da748405f2cb664
SirTenzin <jeffreypfc21@gmail.com>
2021-06-28 20:26:35 +0100

    fix ws 2 PLEASE

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const server = https.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 
+io.set('transports', ['websocket']);
+
 app.use(express.static("./source"))
 
 app.get("/", (req, res) => {
